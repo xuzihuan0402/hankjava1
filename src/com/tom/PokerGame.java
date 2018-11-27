889f2ecc8c4a762829a6a9aa845558207536ec1a
xuzihuan0402 <xu13310101131@gmail.com>
2018-11-27 12:01:34 +0800

    inital commit

---
+++ src/com/tom/PokerGame.java
@@ -0,0 +1,56 @@
+package com.tom;
+
+import java.util.Random;
+
+public class PokerGame {
+
+	public static void main(String[] args) {
+		
+		Random random = new Random();
+		int flowers[]={0x2663,0x2666,0x2665,0x2660};
+		for(int i = 0 ; i <3 ; i++){
+			
+			}
+			}
+		
+		/*
+		int nums[] = new int [5];
+		nums[0] = 77 ;
+		nums[4] = 98 ;
+		System.out.println(nums[4]);
+		System.out.println(nums.length);
+		for(int i = 0 ;i<nums.length ;i++){
+			System.out.println(nums[i]);
+		}
+		*/
+		
+		/*
+		char[]A = {'S','H','D','C'};
+		Random r = new Random();
+		int sub = r.nextInt(A.length);
+		System.out.print(A[sub]);
+		*/
+		
+		/*
+		int num = random.nextInt(13)+1 ;
+		System.out.print(num);
+		char str = 'S';
+		switch(random.nextInt(4)+1){
+		case 1 :
+			str = 'S';
+			break;
+		case 2 :
+			str = 'H';
+			break;
+		case 3 :
+			str = 'D';
+			break;
+		default:,x
+			str = 'C',
+		}
+		System.out.print(str);
+		*/
+
+	}
+
+}
